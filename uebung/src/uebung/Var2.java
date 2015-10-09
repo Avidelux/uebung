31d4fd5468399be6376ef0d65b023fd2693412e4
Lamby.Marcel <Lamby.Marcel@SK-448A5BBA226F>
2015-10-09 09:07:50 +0200

    neu neu

---
+++ uebung/src/uebung/Var2.java
@@ -0,0 +1,17 @@
+package uebung;
+
+public class Var2 {
+
+	public static void main(String[] args) {
+			 int x=17;
+			 int y=4;
+		
+			 System.out.println("x+y = "+(x+y));
+			 x=x+y;
+			 System.out.println("x = "+x) ;
+			 System.out.println("y = "+y) ;
+			 y=x*2;
+			 System.out.println("y = " +y) ;
+	}
+
+}
