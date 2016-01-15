d10ce630a14a5e1cd2413ba806366b8d31f2278e
Lamby.Marcel <Lamby.Marcel@192.168.3.125>
2016-01-15 09:15:05 +0100

    erga

--- uebung/src/uebung/Uebung.java
+++ uebung/src/uebung/Uebung.java
@@ -4,38 +4,34 @@ public class Uebung {
 	
 	public static void main( String[] args) {
 		
-		int [] [] Ar = new int [3] [3];
+		int [] [] Ar = new int [100] [100];
 		
-		Ar [0] [0] = 1;
-		Ar [1] [0] = 2;
-		Ar [0] [1] = 3;
-		Ar [2] [1] = 4;
-		Ar [1] [2] = 5;
-		Ar [0] [2] = 6;
-		Ar [2] [0] = 7;
-		Ar [2] [2] = 8;
-		Ar [1] [1] = 9;
-		
-		
-		
-		
+		int x = 1;
 		
 		
 		for (int i = 0 ; i<Ar.length; i++){
-			
-			
-		
 			for (int j = 0 ; j<Ar[i].length; j++){	
-			System.out.println(Ar[i][j]);
+				Ar [i][j] = x;
+				x++;
+					}
+		     }
 			
-			}
+		for (int i = 0 ; i<Ar.length; i++){
+			for (int j = 0 ; j<Ar[i].length; j++){	
+				System.out.print(Ar[i][j]+" ");
+					}
+		System.out.println();     
 		}
+			
+			
+			
 		
-	
-		System.out.println();
-	
-	
-	
+		
+		
+		
+		
+		
+		
 	}
 
   }
