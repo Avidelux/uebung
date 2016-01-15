d10ee44e9c0bdab50b2e999d298ced886734a082
Lamby.Marcel <Lamby.Marcel@192.168.3.125>
2016-01-15 07:44:28 +0100

    tr

--- uebung/src/uebung/Uebung.java
+++ uebung/src/uebung/Uebung.java
@@ -2,19 +2,7 @@ package uebung;
 
 public class Uebung {
 			
-		static int quadrat(int x) {
-	return x*x;
-		}	
 	
-	public static void main(String[] args) {
-	
-
-	
-			 System.out.println(quadrat(5));
-			 System.out.println(quadrat(10));
-			 System.out.println(quadrat(0));
-			 System.out.println(quadrat(-12));
-
 	}
 
   }
