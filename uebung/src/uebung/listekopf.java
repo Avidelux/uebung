28aba5777ebb6822c0798e1804aae1ed3b36aaca
Lamby.Marcel <Lamby.Marcel@192.168.3.125>
2016-01-19 14:45:08 +0100

    rf

---
+++ uebung/src/uebung/listekopf.java
@@ -0,0 +1,10 @@
+package uebung;
+
+public class listekopf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- uebung/src/uebung/testliste.java
+++ uebung/src/uebung/testliste.java
@@ -5,9 +5,11 @@ public class testliste{
 	public static void main(String [] args){
 	listen erstes = new listen (22);
 	listen zweites = new listen (43);
-	listen dittes = new listen (12);
+	listen drittes = new listen (12);
 	
 	
+	erstes.add(zweites);
+	zweites.add(drittes);
 	
 	
 	}
