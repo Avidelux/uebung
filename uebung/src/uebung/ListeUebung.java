35d2eb410f4e2a1b60294831086c7de00d2ae8ae
Lamby.Marcel <Lamby.Marcel@192.168.3.125>
2016-01-22 08:39:27 +0100

    trgf

--- uebung/src/uebung/ListeUebung.java
+++ uebung/src/uebung/ListeUebung.java
@@ -1,10 +1,27 @@
 package uebung;
+import java.util.ArrayList;
 
 public class ListeUebung {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ArrayList<String> liste  = new ArrayList<String>();
+		liste.add("eins");
+		liste.add("zwei");
+		liste.add("drei");
+		
+		for (int i=0;i<liste.size(); i++){
+			System.out.print(liste.get(i)+", ");
+			
+		}
+		System.out.println();
+		
+		
+		liste.set(0,"leib"); 
+		
+		for (int i=0;i<liste.size(); i++){
+			System.out.print(liste.get(i)+", ");}
 	}
 
+
+	
 }
