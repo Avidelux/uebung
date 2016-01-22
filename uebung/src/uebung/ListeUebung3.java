bdaa6265843c70e1dc78001887f196160948d148
Lamby.Marcel <Lamby.Marcel@192.168.3.125>
2016-01-22 08:43:30 +0100

    rth

---
+++ uebung/src/uebung/ListeUebung3.java
@@ -0,0 +1,34 @@
+package uebung;
+
+		import java.util.ArrayList;
+		import java.util.ListIterator;
+
+public class ListeUebung3 {
+
+	public static void main(String[] args) {
+
+				ArrayList<Integer> zahlen = new ArrayList<Integer>();
+								
+				for(int i = 10; i<=1000; i+=10){
+					zahlen.add(i);
+				}
+				
+				for(int ausgabe : zahlen)
+				{
+					System.out.println(ausgabe);
+				}
+				
+				ListIterator<Integer> li = zahlen.listIterator();
+				 
+				while(li.hasNext()) {
+					System.out.println(li.next());
+				}
+				
+			}
+
+
+		
+		
+	}
+
+
